d4ac9134d87b27602c7e946092d73542a383ca9d
wswebcreation <wim@wswebcreation.nl>
2024-05-29 08:12:22 +0200

    feat: quick adjustment for Jimp
    - added test results

--- README.md
+++ README.md
@@ -10,10 +10,60 @@ I'm running this on a MacBook Pro:
 - Memory: 36GB
 - Power adapter: charging (running on battery takes 30-40% longer)
 
-## Run Canvas tests
+## Testing
+
+Each test will store data in a file
+
+### Run Canvas tests
 
 Run
 
 ```sh
 node run.canvas.js
 ```
+
+Results will be shown in the terminal but also in this file `run.canvas.txt`
+
+### Run Jimp tests
+
+Run
+
+```sh
+node run.jimp.cjs
+```
+
+Results will be shown in the terminal but also in this file `run.jimp.txt`
+
+### Run Visual Tests
+
+- Clone this rep
+  go into the ` resemble`` folder with  `cd resemble`
+- link the module with `npm link`
+- Clone `https://github.com/webdriverio/visual-testing.git`
+- Check out the branch `ws/canvas-2-jimp` with `git checkout ws/canvas-2-jimp`
+- install the dependencies according to the project guidelines
+- go into the `packages/webdriver-image-comparison` folder with `packages/webdriver-image-comparison`
+- link the `resemble-jimp` with `npm link resemble-jimp`
+
+Run one of the following tests and add the results in the txt files
+
+```sh
+# Local setup
+pnpm test.local.init
+# Run local tests
+pnpm test.local.desktop
+
+# Run Storybook and store results in `run.jimp.storybook.txt`
+pnpm test.local.desktop.storybook
+
+# Run SauceLabs web tests and store them in `run.jimp.saucelabs.web.txt`
+pnpm test.saucelabs.web
+
+# Run SauceLabs web tests and store them in `run.jimp.saucelabs.app.txt`
+pnpm test.saucelabs.app
+```
+
+## Preliminary result
+
+1. Comparing the images, without running them with WDIO, results in Jimp (pure JS dependency) being around 70% slower in comparing two images in comparison to using Canvas (a native dependency)
+1. We don't see a huge difference between running WDIO tests with Jimp in comparison to using Canvas, so the 70% slowness might not be seen when running with Jimp
---
+++ run.jimp.cjs
@@ -0,0 +1,95 @@
+const async = require("async");
+const resemble = require("resemble-jimp");
+const fs = require("fs");
+
+let spinner;
+const logFilePath = "run.jimp.txt";
+
+const loadOra = async () => {
+  const oraModule = await import("ora");
+  return oraModule.default;
+};
+const loadHelpers = async () => {
+  const helpersModule = await import("./helpers.js");
+  return helpersModule;
+};
+
+const readImagesFromDirectory = async (directoryPath) => {
+  const { readPngsFromFolder } = await loadHelpers();
+  return readPngsFromFolder({ mainDir: directoryPath });
+};
+
+const compareImages = (image1, image2) => {
+  return new Promise((resolve, reject) => {
+    resemble(image1)
+      .compareTo(image2)
+      .onComplete((data) => {
+        if (data) resolve(data);
+        else reject(new Error("Comparison failed"));
+      });
+  });
+};
+
+const performPerformanceTest = async (images, concurrency) => {
+  let processedCount = 0;
+  let failedCount = 0;
+  const { logToFile } = await loadHelpers();
+
+  const queue = async.queue(async (task) => {
+    try {
+      const actualPath = task;
+      const baselinePath = actualPath.replace("actual", "baseline");
+
+      if (!fs.existsSync(baselinePath)) {
+        throw new Error(`Baseline image not found for ${actualPath}`);
+      }
+
+      const result = await compareImages(actualPath, baselinePath);
+      processedCount++;
+    } catch (error) {
+      failedCount++;
+      logToFile(logFilePath, `Error comparing ${task}: ${error.message}`);
+    }
+  }, concurrency);
+
+  images.forEach((imagePath) => {
+    queue.push(imagePath);
+  });
+
+  return new Promise((resolve) => {
+    queue.drain(() => {
+      spinner.stop();
+      logToFile(
+        logFilePath,
+        `All image comparisons are done. 
+Processed: ${processedCount}, 
+Failed: ${failedCount}`
+      );
+      resolve();
+    });
+  });
+};
+
+const main = async () => {
+  const imagesDirectory = "./images/actual";
+  const concurrency = 10;
+  const { logToFile } = await loadHelpers();
+  // const images = await readImagesFromDirectory(imagesDirectory);
+  const images = [
+    "./images/actual/android_googleapi_emulator/fullPage-EmulatorAndroidGoogleAPILandscapeChromeDriver10.0-640x384.png",
+  ];
+  logToFile(
+    logFilePath,
+    `\nFound ${images.length} images. Starting performance test with concurrency level ${concurrency}.`
+  );
+  const ora = await loadOra();
+  spinner = ora("Processing images...").start();
+  const startTime = Date.now();
+  await performPerformanceTest(images, concurrency);
+  const endTime = Date.now();
+  const duration = (endTime - startTime) / 1000;
+  logToFile(logFilePath, "Done Processing images");
+  logToFile(logFilePath, `Performance test completed in ${duration} seconds.`);
+};
+
+main();
